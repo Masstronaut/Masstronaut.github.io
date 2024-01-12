6985a9e79bb9337dbb69d1e72de5666a851d2266
adblog-buildbot <allan+adblog.buildbot@allandeutsch.com>
2024-01-12 21:23:31 +0000

    Automated site build

---
+++ _app/immutable/chunks/navigation.28eb1a3f.js
@@ -0,0 +1,1 @@
+import{k as a}from"./singletons.fff67f76.js";const i=a("goto"),o=a("invalidate"),l=a("invalidate_all");export{l as a,i as g,o as i};
--- _app/immutable/chunks/paths.5581d5d4.js
+++
@@ -1,1 +0,0 @@
-var s;const e=((s=globalThis.__sveltekit_z2mq54)==null?void 0:s.base)??"";var a;const t=((a=globalThis.__sveltekit_z2mq54)==null?void 0:a.assets)??e;export{t as a,e as b};
