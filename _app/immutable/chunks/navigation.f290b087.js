48815c497a9dd01d3d34d5bb4544ac6bc44148e7
adblog-buildbot <allan+adblog.buildbot@allandeutsch.com>
2024-04-16 19:27:05 +0000

    Automated site build

---
+++ _app/immutable/chunks/navigation.f290b087.js
@@ -0,0 +1,1 @@
+import{k as a}from"./singletons.8bee1b21.js";const i=a("goto"),o=a("invalidate"),l=a("invalidate_all");export{l as a,i as g,o as i};
--- _app/immutable/chunks/navigation.fa028e86.js
+++
@@ -1,1 +0,0 @@
-import{k as a}from"./singletons.cd0c5b7f.js";const i=a("goto"),o=a("invalidate"),l=a("invalidate_all");export{l as a,i as g,o as i};
