835492ba4a39eb5372bda66aae873a3b15572827
adblog-buildbot <allan+adblog.buildbot@allandeutsch.com>
2024-05-11 23:02:23 +0000

    Automated site build

---
+++ _app/immutable/chunks/stores.8e2cde72.js
@@ -0,0 +1,1 @@
+import{d as e}from"./singletons.da04bc14.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
