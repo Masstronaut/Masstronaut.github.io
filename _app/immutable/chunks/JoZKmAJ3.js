88b06f4043bb77949a1b4075ec7a5712cd1de95b
adblog-buildbot <allan+adblog.buildbot@allandeutsch.com>
2025-11-22 19:33:54 +0000

    Automated site build

---
+++ _app/immutable/chunks/JoZKmAJ3.js
@@ -0,0 +1,49 @@
+const e=`---
+title: Seattle area cafés I like
+description: A curated list of my favorite Seattle area cafes to sit, sip, and relax or work.
+slug: notes/seattle-cafes
+publish: true
+lastmod: 2025-10-09
+date: 2021-08-28
+aliases: null
+tags:
+- status/budding
+---
+
+Sometimes while working from home I want to mix things up from the normal work environment at home. Cafes have many perks:
+
+* drinks and small bites
+* They smell fantastic
+* People watching
+* A change of atmosphere/vibe
+* Moving to another location is an easy way to rebuild momentum
+
+These are some of my favorite cafés in the Seattle area.
+
+|Venue|City/neighborhood|Laptop friendly|Notes|
+|-----|-----------------|---------------|-----|
+|Ada's Cafe|Capitol Hill|✅|Technical bookstore with nice indoor/outdoor seating and good tea. Often busy.|
+|[Anchorhead](https://maps.app.goo.gl/HTbuxjQfiAhu8kJq7)|Belltown|⚠️|Good coffee, great mochas, awesome location. No wifi.|
+|[Aroom](https://maps.app.goo.gl/XZekg4WaSP5SN8aP6)|Fremont|✅|Delicious Vietnamese coffee. Seating is limited|
+|[CoffeeTAB](https://maps.app.goo.gl/C8WdZJmY8VbdLnr79)|Belltown|✅|Good coffee with a great cause (helping homeless youth). Quiet w/ good seating.|
+|Don't yell at me|Kirkland|✅|Clean, modern space with solid bbt. UW location is too busy.|
+|[Fulcrum Cafe](https://maps.app.goo.gl/5Jt391bEcs1GaFGB8)|Belltown|✅|Solid coffee and great seasonals. Nice working space|
+|[Goldvine](https://maps.app.goo.gl/96FW6h7KfGp2TK1s7)|Belltown|✅|Puget sound views with excellent coffee and seating. Roasting day smells 💯|
+|Happy Lemon SLU|South Lake Union|✅|Nice outdoor urban area in Summer. Get a tart citrus fruit drink.|
+|[HeyTea](https://maps.app.goo.gl/96QPY6qjLc9CSxLDA)|Bellevue|❌|Easily #1 bubble tea. ❤️ brown sugar bobo & mango coconut boom. No seating|
+|[Little Oddfellows](https://maps.app.goo.gl/Nk3DQswXDPBM4LN28)|Capitol Hill|⚠️|Great vibesy cafe in a bookstore. No wifi, but hotspotting works great.|
+|Lizzy Kate|Kirkland|✅|Great tea shop with indoor & outdoor seating. Limited snack options.|
+|Mercury Coffee Co.|Bellevue|✅|good variety of food & seating options. Excellent cinnamon rolls!|
+|[Millstead & co](https://maps.app.goo.gl/X6ULLyZGNFh23NZx8)|Fremont|✅|Third wave coffee w/ 💯 mocha. Lots of seating, wifi, excellent location for a (s)troll.|
+|Miro Tea|Ballard|✅|large tea selection, including ceremonial matchas. My favorite tea shop. BUSY|
+|[Mintish](https://maps.app.goo.gl/AbsWFmas8PHxXemV6)|Capitol Hill|⚠️|Lovely space with delicious Middle-Eastern inspired flavors. Very slow wifi|
+|Nana's Green Tea|South Lake Union||Tea, sweets, and a few casual Japanese dishes. Great matcha & hojicha|
+|[Saint Bread](https://maps.app.goo.gl/fuFVimoxjLDU7BGv9)|University District|⚠️|Great bakery with immaculate waterfront vibes for Summer. Outdoor only.|
+|[Starbucks Reserve Bakery](https://maps.app.goo.gl/vhCC6iDbk3d82Sp48)|SoDo|✅|The vibe and quality of a Starbucks Reserve without the crowd.|
+|[Storyville Coffee](https://maps.app.goo.gl/bUZdKGNtjxHhF8mK9)|Belltown|✅|Stunning aesthetic with great seating and windows. Overpriced by $1-2 per item.|
+|Tea Republik|University District|✅|nice selection of teas, cheap & quiet. Good cheap food nearby.|
+|[Temple Pastries](https://goo.gl/maps/xrhxY9NHQK5hF4kB7)|Leschi / Central|✅|Excellent pastries, 100mbps wifi, lots of seating. Try the mushroom quiche!|
+|[The Shop by Porter](https://maps.app.goo.gl/JKNoBA4VSgEkEP1W9)|Downtown|✅|Beautifully designed hybrid cowork & cafe space. Probably the best cafe for working.|
+|URL|First Hill|✅|~30-45Mbps wifi, good matcha & lattes, big tables|
+|[Vita at KEXP](https://goo.gl/maps/scrPEj6hh1QfRAbN9)|Seattle Center|✅|Strong menu variety, KEXPs community space is great, and excellent music.|
+`;export{e as default};
